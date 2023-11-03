477e839990f6b8494a8f9747872f2bde17c8ca17
eathyn <eathynzakas@gmail.com>
2023-11-03 16:50:59 +0800

    build: define environment variables

--- webpack-config/base.js
+++ webpack-config/base.js
@@ -127,6 +127,7 @@ module.exports = {
     new DefinePlugin({
       __VUE_OPTIONS_API__: 'false',
       __VUE_PROD_DEVTOOLS__: 'false',
+      'process.env': JSON.stringify(process.env),
     }),
     new CaseSensitivePathsPlugin(),
     new CopyPlugin({
