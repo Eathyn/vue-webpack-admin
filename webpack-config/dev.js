4f0cce1e037c473db56a0e62331f17b911367159
eathyn <eathynzakas@gmail.com>
2023-11-03 16:48:54 +0800

    feat: configure development server proxy

--- webpack-config/dev.js
+++ webpack-config/dev.js
@@ -8,4 +8,12 @@ const style = require('./style')
 module.exports = merge(base, style, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://api.imooc-admin.lgdsunday.club',
+        changeOrigin: true,
+      },
+    },
+  },
 })
